0d2d73c99f2f8cea9c775ea5bce44b339997e182
YaelZamora <ajedrezkasparov@gmail.com>
2023-03-21 10:02:12 -0600

    Initial Commit

---
+++ tinderDribbble/tinderDribbbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  tinderDribbbleApp.swift
+//  tinderDribbble
+//
+//  Created by Yael Javier Zamora Moreno on 21/03/23.
+//
+
+import SwiftUI
+
+@main
+struct tinderDribbbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
